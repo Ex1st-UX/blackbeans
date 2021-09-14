2b780bb61ec50bfa9827d4b0d4fb9c41d68691ee
Ex1st-UX <sneeskaaa@gmail.com>
2021-09-14 08:56:34 +0400

    добваил вывод картинки доставок из бд

--- resources/views/templates/cart.blade.php
+++ resources/views/templates/cart.blade.php
@@ -56,7 +56,7 @@
                                        aria-controls="collapseExample">
                                         <div class="delievery-item-wrapper">
                                             <img class="pochta-russia-logo"
-                                                 src="{{ asset('/storage' . $delieveryItem->image) }}">
+                                                 src="{{ asset('/storage/' . $delieveryItem->image) }}">
                                             <span class="delievery-item-content">
                                             {{ $delieveryItem->name }}
                                             </span>
